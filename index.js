f7c961cb5d08bf04de021b38f98fd53a62f7444b
samanthamarberger <slatham04@gmail.com>
2022-09-20 13:02:02 -0600

    commit tree 3c9f726c6ae8924db80561facd20408c0ef6ad54

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (!activity) {
+        activity = "roller-skate";
+    }
+    let sundayActivity = `This Saturday, I want to ${activity}!`;
+    return sundayActivity;
+}
+
+const mondayWork = function (activity) {
+    if (!activity) {
+        activity = "go to the office";
+    }
+    let mondayActivity = `This Monday, I will ${activity}.`;
+    return mondayActivity;
+}
+
+function wrapAdjective(flair = '*') {
+    return function (are = 'special') {
+        return `You are ${flair}${are}${flair}!`;
+    }
+}
